38b4e71971f0239660bbac6a201e0b3af4d35ca1
Julien Férard <julien_ferard@yahoo.fr>
2017-04-30 22:27:00 +0200

    Fix debug

--- py4lo/debug_command.py
+++ py4lo/debug_command.py
@@ -17,8 +17,13 @@
    You should have received a copy of the GNU General Public License
    along with this program.  If not, see <http://www.gnu.org/licenses/>."""
 
+import os
 from test_command import TestCommand
+from command_executor import CommandExecutor
 import logging
+from script_processor import ScriptProcessor
+from callbacks import *
+from zip_updater import ZipUpdater
 
 class DebugCommand():
     @staticmethod
@@ -26,37 +31,36 @@ class DebugCommand():
         test_executor = TestCommand.create(args, tdata)
         logger = logging.getLogger("py4lo")
         logger.setLevel(tdata["log_level"])
-        debug = DebugCommand(
-            logger,
-            tdata["src_dir"],
-            tdata["target_dir"],
-            tdata["python_version"],
-            tdata["debug_file"],
-            "Creates a debug.ods file with button for each function"
-        )
-        return CommandExecutor(debug, test_executor)
+        debug_command = DebugCommand(logger, tdata["py4lo_path"], tdata["src_dir"], tdata["target_dir"], tdata["python_version"], tdata["debug_file"])
+        return CommandExecutor(debug_command, test_executor)
 
-    def __init__(self, logger, src_dir, target_dir, python_version, ods_dest_name, help_msg):
+    def __init__(self, logger, py4lo_path, src_dir, target_dir, python_version, ods_dest_name):
         self.__logger = logger
+        self.__py4lo_path = py4lo_path
         self.__src_dir = src_dir
         self.__target_dir = target_dir
         self.__python_version = python_version
         self.__ods_dest_name = ods_dest_name
-        self.__help_msg = help_msg
         self.__debug_path = os.path.join(target_dir, ods_dest_name)
 
-
-    def execute(self):
-        self.__logger.info("Debug or init. Generating %s for Python %s", debug_path, python_version)
+    def execute(self, *args):
+        self.__logger.info("Debug or init. Generating %s for Python %s", self.__debug_path, self.__python_version)
 
         script_fnames = set(os.path.join(self.__src_dir, fname) for fname in os.listdir(self.__src_dir) if fname.endswith(".py"))
         script_processor = ScriptProcessor(self.__logger, self.__src_dir, self.__python_version, self.__target_dir)
         script_processor.process(script_fnames)
+        scripts = script_processor.get_scripts()
+        zip_updater = ZipUpdater()
+        (
+            zip_updater
+                .item(ignore_scripts)
+                .item(rewrite_manifest(scripts))
+                .after(add_scripts(scripts))
+                .after(add_debug_content(script_processor.get_exported_func_names_by_script_name()))
+        )
+        zip_updater.update(os.path.join(self.__py4lo_path, "inc", "debug.ods"), self.__ods_dest_name)
+        return (self.__ods_dest_name,)
 
-        item_cbs = [ignore_scripts, rewrite_manifest(script_processor.get_scripts())]
-        after_cbs = [add_scripts(script_processor.get_scripts()), add_debug_content(script_processor.get_exported_func_names_by_script_name())]
-        update_zip(os.path.join(tdata["py4lo_path"], "inc", "debug.ods"), ods_dest_name, item_callbacks = item_cbs, after_callbacks = after_cbs)
-        return ods_dest_name
-
-    def get_help(self):
-        return self.__help_msg
+    @staticmethod
+    def get_help():
+        return "Creates a debug.ods file with button for each function"
