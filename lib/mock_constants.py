dfa9011bd231ec176b77a869247b6debcd53810e
Julien Férard <julien_ferard@yahoo.fr>
2023-07-01 13:57:33 +0200

    Use actual values

--- lib/mock_constants.py
+++ lib/mock_constants.py
@@ -25,10 +25,35 @@ from urllib.parse import urlparse
 ## DO NOT EMBED THIS FILE ##
 ############################
 class DataType(enum.Enum):
-    INTEGER = 0
-    TEXT = 1
-    DATE = 2
+    BIT = -7
+    TINYINT = -6
+    SMALLINT = 5
+    INTEGER = 4
+    BIGINT = -5
+    FLOAT = 6
+    REAL = 7
+    DOUBLE = 8
+    NUMERIC = 2
     DECIMAL = 3
+    CHAR = 1
+    VARCHAR = 12
+    LONGVARCHAR = -1
+    DATE = 91
+    TIME = 92
+    TIMESTAMP = 93
+    BINARY = -2
+    VARBINARY = -3
+    LONGVARBINARY = -4
+    SQLNULL = 0
+    OTHER = 1111
+    OBJECT = 2000
+    DISTINCT = 2001
+    STRUCT = 2002
+    ARRAY = 2003
+    BLOB = 2004
+    CLOB = 2005
+    REF = 2006
+    BOOLEAN = 16
 
 
 class ColumnValue:
