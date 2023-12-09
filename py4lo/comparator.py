22afc637f168e38bbbe93783afccc46d7027200a
Julien Férard <julien_ferard@yahoo.fr>
2023-12-09 11:58:00 +0100

    Type check

--- py4lo/comparator.py
+++ py4lo/comparator.py
@@ -17,7 +17,8 @@
 #     You should have received a copy of the GNU General Public License
 #     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 from abc import ABC, abstractmethod
-from typing import Any, Union, Dict
+from typing import (Any, Dict, Optional, TypeVar, Union, cast)
+from typing_extensions import reveal_type
 
 
 class Comparable(ABC):
@@ -26,44 +27,55 @@ class Comparable(ABC):
         pass
 
 
-T = Union[int, float, str]
+EXPR = Union[int, float, str]
+T = TypeVar("T", int, float, str)
 
 
 class Comparator:
     """A comparator will be used to evaluate expressions. Used by the branch
     processor"""
 
-    def __init__(self, accepted_locals: Dict[str, T] = None):
+    def __init__(self, accepted_locals: Optional[Dict[str, T]] = None):
         """accepted_locals are """
         if accepted_locals is None:
             self._accepted_locals = {}
         else:
             self._accepted_locals = accepted_locals
 
-    def check(self, arg1: T, comparator: str, arg2: T):
+    def check(self, arg1: EXPR, comparator: str, arg2: EXPR):
         """Check arg1 vs arg2 using comparator. Args may be $var where var is
         a member of accepted_locals, a number 123456i, 123.456f, an expression
         or a litteral."""
-        arg1 = self._parse_expr(arg1)
-        arg2 = self._parse_expr(arg2)
-        cmp_result = self._cmp(arg1, arg2)
+        parsed_arg1 = self._parse_expr(arg1)
+        parsed_arg2 = self._parse_expr(arg2)
+        if type(parsed_arg1) is not type(parsed_arg2):
+            return False
+
+        cmp_result = self._cmp(parsed_arg1, parsed_arg2) # type: ignore [type-var]
         return (cmp_result == -1 and comparator in ["<", "<="]
                 or cmp_result == 0 and comparator in ["<=", "==", ">="]
                 or cmp_result == 1 and comparator in [">", ">="])
 
-    def _parse_expr(self, expr: str) -> T:
+    def _parse_expr(self, expr: EXPR) -> EXPR:
+        if not isinstance(expr, str):
+            return expr
+
+        # substitution
         if expr[0] == '$':
             name = expr[1:]
             expr = eval(name, {'__builtin__': None}, self._accepted_locals)
+
         if not isinstance(expr, str):
             return expr
-        try:
-            if expr[-1] == 'f':
-                return float(expr[0:-1])
-            elif expr[-1] == 'i':
-                return float(expr[0:-1])
-        except ValueError:
-            pass
+
+        if expr:
+            try:
+                if expr[-1:] == 'f':
+                    return float(expr[0:-1])
+                elif expr[-1:] == 'i':
+                    return float(expr[0:-1])
+            except ValueError:
+                pass
 
         return expr
 
