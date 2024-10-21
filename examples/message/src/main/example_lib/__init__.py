b27eb6514f9fc810e2aa961ea9d308ae04a56c6e
Julien Férard <julien_ferard@yahoo.fr>
2024-10-21 18:22:46 +0200

    Add mypy flag

--- examples/message/src/main/example_lib/__init__.py
+++ examples/message/src/main/example_lib/__init__.py
@@ -16,8 +16,10 @@
 #     You should have received a copy of the GNU General Public License
 #     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import unohelper
+# mypy: disable-error-code="import-untyped"
 import os
+
+import unohelper
 
 from alib import TEXT
 from py4lo_dialogs import message_box
