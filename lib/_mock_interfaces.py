d1e9a86249c58f59b2af1fd99d8a4a91047907cf
Julien Férard <julien_ferard@yahoo.fr>
2024-12-27 14:24:37 +0100

    Rename mock_constants and add mock_interfaces

---
+++ lib/_mock_interfaces.py
@@ -0,0 +1,110 @@
+#  Py4LO - Python Toolkit For LibreOffice Calc
+#     Copyright (C) 2016-2022 J. Férard <https://github.com/jferard>
+#
+#     This file is part of Py4LO.
+#
+#     Py4LO is free software: you can redistribute it and/or modify
+#     it under the terms of the GNU General Public License as published by
+#     the Free Software Foundation, either version 3 of the License, or
+#     (at your option) any later version.
+#
+#     Py4LO is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#     GNU General Public License for more details.
+#
+#     You should have received a copy of the GNU General Public License
+#     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+##############################
+# # DO NOT EMBED THIS FILE # #
+##############################
+
+class unohelper:
+    class Base:
+        pass
+
+
+class uno:
+    pass
+
+# Listeners
+
+class XActionListener:
+    pass
+
+
+class XActivateListener:
+    pass
+
+
+class XAdjustmentListener:
+    pass
+
+
+class XDockableWindowListener:
+    pass
+
+
+class XFocusListener:
+    pass
+
+
+class XItemListener:
+    pass
+
+
+class XItemListListener:
+    pass
+
+
+class XKeyListener:
+    pass
+
+
+class XMenuListener:
+    pass
+
+
+class XMouseListener:
+    pass
+
+
+class XMouseMotionListener:
+    pass
+
+
+class XPaintListener:
+    pass
+
+
+class XSpinListener:
+    pass
+
+
+class XStyleChangeListener:
+    pass
+
+
+class XTabListener:
+    pass
+
+
+class XTextListener:
+    pass
+
+
+class XTopWindowListener:
+    pass
+
+
+class XVclContainerListener:
+    pass
+
+
+class XWindowListener:
+    pass
+
+
+class XWindowListener2:
+    pass
